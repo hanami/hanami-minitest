5338f516bacbf2ef5cbb07fca374ccd94a5d585d
Tim Riley <tim@riley.id.au>
2026-03-20 08:58:24 +1100

    Finesse wording

--- lib/hanami-minitest.rb
+++ lib/hanami-minitest.rb
@@ -2,11 +2,11 @@
 
 require_relative "hanami/minitest"
 
-# Require Rake tasks last, after hanami/minitest is fully loaded.
+# Load Rake tasks last, after hanami/minitest is fully loaded, to avoid circular requires.
 #
-# When a Hanami app's Rakefile loads, Hanami CLI requires all gems in the `:cli` Bundler group,
-# which is what requires this file. If rake_tasks were required from inside hanami/minitest.rb, that
-# file would still be mid-load when the user runs `rake test`, which will load the app's
-# test_helper.rb, which itself does a require for "hanami/minitest", triggering a circular require
-# warning.
+# When you run `rake test` in a Hanami app, the Rakefile will first require all gems in the `:cli`
+# Bundler group, via Hanami CLI. If we required rake_tasks inside hanami/minitest.rb, that file
+# would still be mid-load when the Rake test runner loads the test files. Since each test file
+# requires test_helper.rb, which itself does `require "hanami/minitest"`, this would otherwise
+# lead to a circular require warning.
 require_relative "hanami/minitest/rake_tasks"
