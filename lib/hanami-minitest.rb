b73c45a929d01310f7e7a3b37cb9cd072c0baf67
Tim Riley <tim@riley.id.au>
2026-03-19 23:06:25 +1100

    Explain the location of the require for rake_tasks

--- lib/hanami-minitest.rb
+++ lib/hanami-minitest.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "hanami/minitest"
+
+# Require Rake tasks last, after hanami/minitest is fully loaded.
+#
+# When a Hanami app's Rakefile loads, Hanami CLI requires all gems in the `:cli` Bundler group,
+# which is what requires this file. If rake_tasks were required from inside hanami/minitest.rb, that
+# file would still be mid-load when the user runs `rake test`, which will load the app's
+# test_helper.rb, which itself does a require for "hanami/minitest", triggering a circular require
+# warning.
 require_relative "hanami/minitest/rake_tasks"
